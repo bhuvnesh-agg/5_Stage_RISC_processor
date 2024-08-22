ebe3496248e61fbc0b784f3c9c1bd05f5dab8f81
Bhuvnesh Aggarwal <72073761+bhuvnesh-agg@users.noreply.github.com>
2024-08-22 19:28:28 +0530

    linear search assembly file

---
+++ linear_search.asm
@@ -0,0 +1,26 @@
+  ADDI  R4  R0  0b0000000000001100  
+  ADDI  R5  R0  0b0000000000000110 
+  ADD   R7  R0  R0              
+
+  .Loop:
+  LW    R7  R3  101
+  ADD   R0  R0  R0
+  ADD   R0  R0  R0
+  SUB   R1  R5  R3
+  ADD   R0  R0  R0
+  ADD   R0  R0  R0
+  BEQZ  R1  .Found
+  SUB   R9  R4  R7
+  ADD   R0  R0  R0
+  ADD   R0  R0  R0
+  BEQZ  R9  .Not_Found
+  ADDI  R7  R7  0b0000000000000001 
+  BNEQZ R1  .Loop
+
+  .Found:
+  ADDI  R31 R0  0b0000000000000001
+  HLT
+
+  .Not_Found:
+  ADDI  R31 R0  0b0000000000000100
+  HLT
